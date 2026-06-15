3c99349f2b4e39af664d08e43b42e9d0cf4a83a2
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2026-06-15 16:25:29 -0400

    Updating and finalizing poc-helm-charts

--- scripts/k8s-init.sh
+++ scripts/k8s-init.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+#source ~/.zshrc
+echo $REGISTRY1_PASSWORD
+
 kubectl create namespace datahub
 
 kubectl create secret docker-registry dockerhub \
@@ -17,9 +20,24 @@ kubectl create secret docker-registry ironbank-pull-secret \
   --docker-email="$REGISTRY1_EMAIL" \
   -n datahub
 
-
 kubectl create secret generic postgresql-secrets \
     --from-literal=postgres-password=datahub \
     --from-literal=password=datahub \
     --from-literal=replication-password=datahub \
+    -n datahub
+
+kubectl create secret docker-registry dockerhub \
+  --docker-server=https://index.docker.io/v1/ \
+  --docker-username="$DOCKERHUB_USERNAME" \
+  --docker-password="$DOCKERHUB_TOKEN" \
+  --docker-email="$DOCKERHUB_EMAIL"
+
+kubectl create secret docker-registry ironbank-pull-secret \
+  --docker-server=registry1.dso.mil \
+  --docker-username="$REGISTRY1_USERNAME" \
+  --docker-password="$REGISTRY1_PASSWORD" \
+  --docker-email="$REGISTRY1_EMAIL"
+
+helm install prerequisites datahub/datahub-prerequisites \
+    --values ./charts/prerequisites/values.yaml \
     -n datahub
